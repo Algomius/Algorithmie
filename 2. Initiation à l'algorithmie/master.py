12f938a70e5d542833adca51912b81bbffaf0d47
Algomius <schlegel.philippe@free.fr>
2023-09-21 16:20:24 +0200

    Premier commit

---
+++ 2. Initiation à l'algorithmie/master.py
@@ -0,0 +1,8 @@
+combinaison = 67451
+print("Entrer une proposition : ")
+proposition = int(input())
+while combinaison != proposition:
+    print("Entrer une proposition : ")
+    proposition = int(input())
+
+print("GAGNE")
